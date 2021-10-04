86dc874ec29f8cba7aba611801e995dc501daafe
werter bonfim <werter@hotmail.com.br>
2021-10-03 22:10:57 -0300

    add dependency

---
+++ Werter.LoggingReferences/Werter.LoggingReferences.SerilogReference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Werter.LoggingReferences.SerilogReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
